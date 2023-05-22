f61273ebf7cd0ad8ac6a52a2e3e692314c8518f7
Harshit chawla <harshitchawla1991@yahoo.com>
2023-05-22 15:21:59 +0530

    Create arrReduce.js

---
+++ polyfills/arrReduce.js
@@ -0,0 +1,14 @@
+Array.prototype.myReduce = function (callback, initialValue) {
+    let accumulator = initialValue;
+    for (let i = 0; i < this.length; i++) {
+      if (accumulator) {
+        accumulator = callback(accumulator, this[i], i, this);
+      } else {
+        accumulator = this[i];
+      }
+    }
+    return accumulator;
+  };
+  const arr = [1, 2, 3, 4];
+  console.log(arr.myReduce((total, elem) => total + elem));
+  console.log(arr.myReduce((prod, elem) => prod * elem));
