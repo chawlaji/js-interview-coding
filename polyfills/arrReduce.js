c0e894d0db0a5bde7605a4b5b1d10899cd2d4c03
Harshit chawla <harshitchawla1991@yahoo.com>
2023-05-22 15:31:47 +0530

    Update arrReduce.js

--- polyfills/arrReduce.js
+++ polyfills/arrReduce.js
@@ -2,7 +2,7 @@ Array.prototype.myReduce = function (callback, initialValue) {
     let accumulator = initialValue;
     for (let i = 0; i < this.length; i++) {
       if (accumulator) {
-        accumulator = callback(accumulator, this[i], i, this);
+        accumulator = callback.call(undefined,accumulator, this[i], i, this);
       } else {
         accumulator = this[i];
       }
