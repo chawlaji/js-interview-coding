2cd43f3430303212789b6f63182db3deb3ca1956
Harshit chawla <harshitchawla1991@yahoo.com>
2023-10-09 15:43:08 +0530

    Update slice.js

--- polyfills/slice.js
+++ polyfills/slice.js
@@ -16,3 +16,25 @@ var arr = [1, 2, 3];
 // Array.prototype.slice
 var sliced = arr.slice(1);
 console.log(sliced); // 
+
+
+// basic implemenatation
+// const slicez =  (array,start , end) => {
+//     const length = array.length;
+//      start = start >= 0 ? start :0;
+//      end = end ?? length;
+  
+//       const result = [];
+//       for (let i = start; i < end; i++) {
+//         result[result.length] = array[i];
+//       }
+//       return result;
+//     };
+
+const slicez =  (array,start = 0, end= array.length) => {
+      const result = [];
+      for (let i = start; i < end; i++) {
+        result[result.length] = array[i];
+      }
+      return result;
+    };
